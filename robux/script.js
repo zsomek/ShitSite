988f0e05fb9babee00f865b1bf393337f417b951
zsomek <101420662+zsomek@users.noreply.github.com>
2022-08-11 19:12:46 +0200

    Add files via upload

--- robux/script.js
+++ robux/script.js
@@ -8,16 +8,9 @@ slider.oninput = function() {
 
 function proceed() {
     var username = document.getElementById("username").value;
-    var submitOK = true
     if (username.length < 1) {
         alert("Please enter a username");
         return false;
-    } 
-    const parent = document.getElementById("body");
-    const child = document.getElementById("page");
-    const para = document.createElement("h1");
-    const node = document.createTextNode("You got rickrolled... better luck next time!");
-    para.appendChild(node);
-    parent.replaceChild(para,child);
-    window.open("https://youtu.be/dQw4w9WgXcQ");
+    }
+    location.href = "assets/rickroll.mp4"
 }
